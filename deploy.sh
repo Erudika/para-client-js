37b0206dc2c631bc5465db9982e8e4e94f58626d
Alex Bogdanovski <alex@erudika.com>
2025-12-26 14:49:15 +0200

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ sed -i -e "s/\"version\":.*/\"version\": "\"$ver\"",/g" package.json
 git add -A && git commit -m "Release v$ver."
 git tag "v$ver"
 git push origin master && git push --tags
-npm run prepare && npm publish
+npm publish
