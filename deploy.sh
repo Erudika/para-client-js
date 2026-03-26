a5d5ad7087a0bf1e4d02719a5b1680a02cbe03dd
Alex Bogdanovski <alex@erudika.com>
2026-03-26 13:35:21 +0200

    Release 1.41.2.

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ echo "Last tag was:" $(git describe --tags --abbrev=0)
 read -e -p "Tag: " ver
 # sed -i -e "s/\"version\":.*/\"version\": "\"$ver\"",/g" package.json
 git add -A && git commit -m "Release $ver."
-git tag "$ver"
+npm version $ver && git tag "$ver"
 git push origin master && git push --tags
 # npm publish
