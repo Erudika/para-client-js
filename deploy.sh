507d0e21ce8e8101075d8a1ca642d493aad206b1
Alex Bogdanovski <alex@erudika.com>
2026-03-26 12:27:30 +0200

    prepare release

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-read -e -p "Tag: " ver
-sed -i -e "s/\"version\":.*/\"version\": "\"$ver\"",/g" package.json
-git add -A && git commit -m "Release v$ver."
-git tag "v$ver"
-git push origin master && git push --tags
-#npm publish
+
+# read -e -p "Tag: " ver
+# sed -i -e "s/\"version\":.*/\"version\": "\"$ver\"",/g" package.json
+# git add -A && git commit -m "Release v$ver."
+# git tag "v$ver"
+# git push origin master && git push --tags
+# npm publish
+
+npm run build && \
+git add -A && git commit -am "prepare release" && \
+npm run release
+
