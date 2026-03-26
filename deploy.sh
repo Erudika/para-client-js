887400bb37e8a71adb9d9de12e26de75821a390f
Alex Bogdanovski <alex@erudika.com>
2026-03-26 13:58:54 +0200

    prepare release

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 echo "Last tag was:" $(git describe --tags --abbrev=0)
-read -e -p "Tag: " ver
-# sed -i -e "s/\"version\":.*/\"version\": "\"$ver\"",/g" package.json
-git add -A && git commit -m "Release $ver."
-npm version $ver && git tag "$ver"
+read -e -p "(p)atch or (m)inor? P/m" ver
+git add -A && git commit -m "prepare release"
+if [[ "$ver" = "m" ]]; then
+	npm version minor
+else
+	npm version patch
+fi
 git push origin master && git push --tags
-# npm publish
+
