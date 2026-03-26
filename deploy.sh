9e3f40e7e717ac2e3ec7dc90a2356f02d75f1a0b
Alex Bogdanovski <alex@erudika.com>
2026-03-26 16:53:36 +0200

    prepare release

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 echo "Last tag was:" $(git describe --tags --abbrev=0)
-read -e -p "(p)atch or (m)inor? P/m" ver
 git add -A && git commit -m "prepare release"
-if [[ "$ver" = "m" ]]; then
-	npm version minor
-else
-	npm version patch
-fi
-git push origin master && git push --tags
+npm run release
 
